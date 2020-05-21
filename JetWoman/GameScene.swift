22c6d237d3af326df546a1e478c39515852d6ff2
Aleksandr Nikiforov <alex@alexn.ee>
2020-05-21 06:23:03 +0300

    Start game button and score

--- JetWoman/GameScene.swift
+++ JetWoman/GameScene.swift
@@ -15,8 +15,11 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     let spikesCategory : UInt32 = 3
     
     private var label : SKLabelNode?
+    private var scorelabel: SKLabelNode?
     private var jetWoman: SKSpriteNode?
     private var startButton: SKSpriteNode?
+    private var score = 0
+    private var gameFinished = true
     
     override func didMove(to view: SKView) {
         
@@ -24,6 +27,8 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         
         // Get label node from scene and store it for use later
         self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
+        self.scorelabel = self.childNode(withName: "//scorelabel") as? SKLabelNode
+        
         if let label = self.label {
             label.alpha = 0.0
             label.run(SKAction.fadeIn(withDuration: 2.0))
@@ -39,23 +44,36 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         let nodesAtPoint = nodes(at: point)
         for node in nodesAtPoint {
             if node.name == "startbutton" {
-                if let jetWoman = self.jetWoman {
-                    jetWoman.physicsBody?.pinned = false
-                    node.removeFromParent()
-                }
+                startGame()
+                node.removeFromParent()
             }
         }
     }
     
+    func startGame() {
+        if let jetWoman = self.jetWoman {
+            jetWoman.position = CGPoint(x: 0, y: 90)
+            jetWoman.physicsBody?.pinned = false
+            gameFinished = false
+            score = 0
+            updateScoreLabel()
+        }
+    }
     override func keyDown(with event: NSEvent) {
         switch event.keyCode {
         case 0x31:
             if let jetWoman = self.jetWoman {
                 jetWoman.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 200))
+                score += 1
+                updateScoreLabel()
             }
         default:
             print("keyDown: \(event.characters!) keyCode: \(event.keyCode)")
         }
+    }
+    
+    func updateScoreLabel() {
+        self.scorelabel?.text = "Score: \(score)"
     }
     
     func didBegin(_ contact: SKPhysicsContact) {
@@ -63,7 +81,12 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         let bodyB = contact.bodyB
         if bodyA.categoryBitMask == spikesCategory || bodyB.categoryBitMask == spikesCategory
         {
-           print("Ran into spikes!")
+            if (!gameFinished) {
+                gameFinished = true
+                if let startButton = self.startButton {
+                    addChild(startButton)
+                }
+            }
         }
     }
     
